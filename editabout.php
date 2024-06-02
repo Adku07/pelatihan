2fede350f06b0ea8467d3cf2911f87dd9c4c8efd
Adku07 <110002242+Adku07@users.noreply.github.com>
2024-06-03 02:12:28 +0700

    Update editabout.php

--- editabout.php
+++ editabout.php
@@ -24,7 +24,9 @@ include 'koneksi.php';
 </head>
 <body>
    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
-   <div class="masterhead
+   <div class="masterhead" style="background-image: url(SVG.png);height: 200px;">
+      <div class="d-flex justify-content-center align-items-center">
+         <img style="height: 200px;" src="pict\logo.png" alt="">
       </div>
   <div class="container">
   
