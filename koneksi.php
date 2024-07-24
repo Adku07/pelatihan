b81d43197b6db14f2508a9f6c060d4568729f89e
Adku07 <110002242+Adku07@users.noreply.github.com>
2024-07-24 12:45:03 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,8 +1,12 @@
 <?php
+$database = 'pelatihan_db';
+$username = 'pelatihan';
+$password = 'CPt+l3y36Ic-5Ai+3I';
+$server = 'localhost';
 
-$koneksi = mysqli_connect('localhost', 'root', '', 'pelatihan');
+$koneksi = mysqli_connect($server, $username, $password, $database);
 
-if (!$koneksi){
-    die('Gagal terkoneksi ke database' . mysqli_connect_error());
+
+if (!$koneksi) {
+    die("Koneksi gagal: " . mysqli_connect_error());
 }
-?>
